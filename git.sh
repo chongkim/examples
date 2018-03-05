f58684a6d93f85a6b22d70d35f43d089eb530f0a
Chong Kim <chong.kim@mineralsoft.com>
2018-03-05 14:41:19 -0600

    git config, git rebase

--- git.sh
+++ git.sh
@@ -6,3 +6,14 @@ put_existing_repository_to_github() {
   git remote add origin git@github.com:chongkim/examples.git
   git push -u origin master
 }
+
+global_config() {
+  git config --global alias.co checkout
+  git co master  # will do `git checkout master`
+}
+
+rebase() {
+  # only do this on your own private branch
+  git rebase other_branch
+  git push -f
+}

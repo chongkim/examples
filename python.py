51baac95af392a130afcfaeb23b0805beba33c59
Chong Kim <chong.kim@mineralsoft.com>
2018-03-06 13:04:09 -0600

    python re.split

--- python.py
+++ python.py
@@ -1,3 +1,11 @@
+# """
+# Example file for python.
+
+# To run from command line:
+#     python -m unittest python.py
+#     python -m unittest python
+#     python -m unittest python.MockTest
+# """
 from unittest import TestCase
 from unittest.mock import Mock
 
@@ -10,3 +18,10 @@ class MockTest(TestCase):
         args, kwargs = f.call_args
         self.assertEqual(args, ('a', 'b', 'c'))
         self.assertEqual(kwargs, {'foo': 'bar'})
+
+
+class ReTest(TestCase):
+    def test_split(self):
+        import re
+        lst = re.split(r'-', 'a-b--c')
+        self.assertEqual(lst, ['a', 'b', '', 'c'])

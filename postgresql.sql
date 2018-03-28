c64c19c2e14731c418565e1b8327067205b33488
Chong Kim <chong.kim@mineralsoft.com>
2018-03-28 13:33:35 -0500

    postgresql show process, kill process

---
+++ postgresql.sql
@@ -0,0 +1,8 @@
+select * from pg_stat_activity;
+
+select pg_cancel_backend(__pid__);
+
+-- list those commands from psql
+select * from pg_stat_activity where application_name='psql';
+
+select pg_terminate_backend(__pid__);

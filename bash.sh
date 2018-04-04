dff0812f94cd2e2b587d0deaf9c1d56747e17959
Chong Kim <chong.kim@mineralsoft.com>
2018-04-04 11:56:32 -0500

    bash: change_hostname

--- bash.sh
+++ bash.sh
@@ -6,3 +6,11 @@ ag --ignore dirname pattern
 ctags -R --exclude=pattern .
 
 ssh-keygen -R "hostname"  # removes hostname from ~/.ssh/known_hosts
+
+change_hostname() {
+  sudo -s
+  vi /etc/hostname  # replace line with the hostname you want (ie: staging)
+  vi /etc/hosts  # add the hostname to the 127.0.0.1
+  hostname staging
+  exit  # don't need to be superuser anymore
+}
